dc2c38e05edbe38414e8390c3effb909f00c63f4
Kotassin <nastya_navros@mail.ru>
2017-07-15 16:12:23 +0300

    hjfg

--- RSA/Source.cpp
+++ RSA/Source.cpp
@@ -5,11 +5,38 @@
 #include <fstream>
 #include <string>
 #include <cstdio>
+#include <math.h>
 
 using namespace std;
 
+long toEncrypt (long message,long e, long mod){
+	long acc = 1, z = message % mod;
+	while (e) {
+		if (e& 1)
+			acc = (acc * z) % mod;
+		z = (z * z) % mod;
+		e >>= 1;
+	}
+	cout << " Message: " << acc;
+	return acc;
+}
+
+	
+long toDecrypt(long message, long d, long mod) {
+		long acc = 1, z = message % mod;
+		while (d) {
+			if (d & 1)
+				acc = (acc * z) % mod;
+			z = (z * z) % mod;
+			d >>= 1;
+		}
+		cout << " Message: " << acc;
+		return acc;
+}
+
+
 int main() {
-	long p, q, m, phi;
+	long p, q, m, phi, e;
 	cout << "Please, input p: ";
 	cin >> p;
 	cout << "Please, input q: "; 
@@ -22,7 +49,13 @@ int main() {
 	cout << " Phi(m) = " << phi << endl;
 
 	cout << " Please, input e:";
+	cin >> e;
 
+	long message = 7, d = 7, mod = 11;
+	toDecrypt (message, d, mod);
+	toEncrypt(message, e, mod);
+
+	
 	system("pause");
 	return 0;
 }
