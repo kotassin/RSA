963ab7da21de11a537cdbad73d1d1c641cb98ef8
Cyryl Bulbum <killertom1997@gmail.com>
2017-07-15 16:05:36 +0300

    Almost ready

--- RSA/Source.cpp
+++ RSA/Source.cpp
@@ -1,15 +1,40 @@
-
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <fstream>
 #include <string>
-#include <cstdio>
 
 using namespace std;
 
+
+long GCD(long a, long b) {
+		while (a && b)
+			if (a >= b)
+				a %= b;
+			else
+				b %= a;
+		return a | b;
+}
+void toCountExtendedEuclid(long a, long b, long *x, long *y, long *d){
+	long q, r, x1, x2, y1, y2;
+	if (b == 0) {
+		*d = a, *x = 1, *y = 0;
+		return;
+	}
+
+	x2 = 1, x1 = 0, y2 = 0, y1 = 1;
+
+	while (b > 0) {
+		q = a / b, r = a - q * b;
+		*x = x2 - q * x1, *y = y2 - q * y1;
+		a = b, b = r;
+		x2 = x1, x1 = *x, y2 = y1, y1 = *y;
+	}
+
+	*d = a, *x = x2, *y = y2;
+}
+long toDigitalCode(string message) {
+   
+}
 int main() {
-	long p, q, m, phi;
+	long p, q, m, phi, k, gcd, e, d;
 	cout << "Please, input p: ";
 	cin >> p;
 	cout << "Please, input q: "; 
@@ -21,8 +46,33 @@ int main() {
 	phi = (p - 1)*(q - 1);
 	cout << " Phi(m) = " << phi << endl;
 
-	cout << " Please, input e:";
+	do
+	{
+		cout << "Please, input e, mutual prime with phi(m): ";
+		cin >> e;
+	} while ((GCD(e, phi) != 1) || (e > phi));
+	
+	toCountExtendedEuclid(phi, e, &k, &d, &gcd);
+	if (d < 0) d += phi;
+	cout <<"Counted d = "<< d <<". Where e*d (mod phi(m)) = "<<(e*d)%phi;
 
+	int todo;
+	cout << endl << endl << "Secret and open key are made. Let's make conversation!"<<endl;
+	cout << "Choose, what you wanna do: " << endl << "Input 1 - to encrypt" << endl << "Input 2 - to decrypt" << endl << endl << "To exit - input 0." << endl << "Your choice: ";
+	cin >> todo;
+
+	while (todo != 0) {
+		switch (todo)
+		{
+		case 0: { break; }
+		case 1: { break; }
+		case 2: { break; }
+		default: { break; }
+		}
+	}
+	
+
+	cout << endl;
 	system("pause");
 	return 0;
 }
